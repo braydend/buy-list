61a1d2078511e1d1bb9e577435e8871abdb867ba
Bray <braydendiluca@gmail.com>
2025-02-08 11:05:59 +1000

    Split lib into client and common

--- src/lib/client/forms/CreateItem.svelte
+++ src/lib/client/forms/CreateItem.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
+	import Button from '$lib/client/components/Button.svelte';
 
 	const { success, error }: { success?: boolean; error?: string } = $props();
 
