95a04c6c72c64b2e656e60ec7a24afe7f1a5f4ef
Bray <braydendiluca@gmail.com>
2025-02-09 20:51:58 +1000

    add Input component to centralise input styling

---
+++ src/lib/client/components/Input.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	type Props = HTMLInputAttributes & {
+		error?: string;
+		name: Required<HTMLInputAttributes['name']>;
+		type: Required<HTMLInputAttributes['type']>;
+	};
+
+	const { name, type, error, placeholder, value = $bindable(), ...props }: Props = $props();
+</script>
+
+<input {type} {name} {placeholder} {value} class={error && 'border border-red-400'} {...props} />
+
+{#if error}
+	<p style="color: red">{error}</p>
+{/if}
