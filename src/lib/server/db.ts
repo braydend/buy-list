d3dc9eddb258a3f8c5a4708869bed575f2e63c52
Bray <braydendiluca@gmail.com>
2025-02-11 10:25:49 +1100

    Update turso env var access

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./db/schema";
+import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "$env/static/private";
 
 export const db = drizzle({
     connection: {
-        url: process.env.TURSO_DATABASE_URL ?? "Missing TURSO_DATABASE_URL",
-        authToken: process.env.TURSO_AUTH_TOKEN ?? "Missing TURSO_AUTH_TOKEN",
+        url: TURSO_DATABASE_URL ?? "Missing TURSO_DATABASE_URL",
+        authToken: TURSO_AUTH_TOKEN ?? "Missing TURSO_AUTH_TOKEN",
     },
     schema,
 });
